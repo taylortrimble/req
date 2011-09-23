920342be99991275aed830891d189f2a4ea46122
Taylor Trimble <tylrtrmbl@gmail.com>
2011-09-23 10:16:18 -0500

    Added basic functionality
    Checks for CLI arguments
    Checks for correct number parsing
    Finds inverse of inverse sum of each argument
    Prints to screen with 4 decimal points
    
    Using libgen.h library for basename() function to parse program name

--- req/main.c
+++ req/main.c
@@ -7,12 +7,34 @@
 //
 
 #include <stdio.h>
+#include <libgen.h>
 
 int main (int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    // Check that arguments are present
+    if (argc < 2) {
+        printf("usage: %s r1 r2 ...\n", basename((char *)argv[0]));
+        return -1;
+    }
+    
+    // Find inverse sum of each argument
+    double inverseSum = 0;
+    for (int i = 1; i < argc; i++) {
+        double arg;                                     // Parsed argument
+        int succeeded = sscanf(argv[i], "%lf", &arg);   // succeeded = 1 if parse succeeded
+        if (!succeeded) {
+            printf("%s: \"%s\" is not a number\n", basename((char *)argv[0]), argv[i]);
+            printf("usage: %s r1 r2 ...\n",basename((char *)argv[0]));
+            return -1;
+        }
+        
+        inverseSum += 1/arg;
+    }
+    
+    // Find and print equivalent reistance
+    double equivalentResistance = 1/inverseSum;
+    printf("%.4f\n", equivalentResistance);
+    
     return 0;
 }
 
